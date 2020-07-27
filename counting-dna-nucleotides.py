21d32d1b2294d1c62c9720e4cfbb0ee7999012e3
TJ Ward <jward3@email.unc.edu>
2020-07-27 10:59:11 -0400

    Add bio notes to solutions

--- counting-dna-nucleotides.py
+++ counting-dna-nucleotides.py
@@ -1,3 +1,9 @@
+# Rosalind ID: DNA
+# NOTES: Necleic acids are a type of macromolecule contained in the necleus of a cell.
+#        Nucleic acides are polymers, repeating chains of similarly structured molecules known as monomers
+#        A monomer ina nucleic acid is known as a nucleotide and is used as a unit of strand (nucleotide) length
+#        DNA is a type of nucleic acid (deoxyribose necleic acid) with phosphate bases consisting of adenine (A), cytosine (C), guanine (G), and thymine (T)
+
 # best answer
 # * expands iterator to function arguments
 # * input() waits for input from the user.
--- transcribing-dna-into-rna.py
+++ transcribing-dna-into-rna.py
@@ -1,1 +1,6 @@
+# Rosalind ID: RNA
+# Notes: RNA (ribose necleic acid) is a nucelotide that "lives alongside" DNA in the chromatim.
+#        DNA and RNA have the same structure EXEPT that uracil (U) replaces all thymine (T).
+#        DNA is trascribed into messenger RNA (mRNA) by copying each nucelotide, replacing T with U.
+
 print(input().replace('T', 'U'))
